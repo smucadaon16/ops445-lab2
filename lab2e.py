2580ca7e661fef7f3f87d595cfb2746bab62aaf1
Sheila Cadaon <smcadaon@gmail.com>
2025-02-01 20:20:20 -0500

    lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Author: Sheila Mae Cadaon
+# Author ID: 152944229
+# Date Created: 2025/02/01
+
+timer = 10
+
+while timer != 0:
+	print(timer)
+	timer = timer - 1
+print('blast off!')
