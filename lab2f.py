8c83caf793b6ea3a38b9dbdcfc565283ac9d8008
Sheila Cadaon <smcadaon@gmail.com>
2025-02-01 20:26:37 -0500

    lab2f.py

---
+++ lab2f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Author: Sheila Mae Cadaon
+# Author ID: 152944229
+# Date Created: 2025/02/01
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+	print(timer)
+	timer = timer - 1
+print('blast off!')
